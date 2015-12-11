2d5367919d6c9b1b416fbadfc246b5e2682023ea
simonsattin <ssattin@gmail.com>
2015-12-11 13:41:49 +0000

    added initial ordersdataaccess

--- IGTradeManager.UI/Program.cs
+++ IGTradeManager.UI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IGTradeManager.UI.Views.MainWindow;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,8 @@ namespace IGTradeManager.UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            
+
+            Application.Run(new MainWindowView());
         }
     }
 }
