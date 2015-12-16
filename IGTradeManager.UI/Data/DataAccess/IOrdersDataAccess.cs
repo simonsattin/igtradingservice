3e65b4168978c02cd5f92db1fc24b9b2b39ca37c
simonsattin <ssattin@gmail.com>
2015-12-16 12:59:33 +0000

    added data access

---
+++ IGTradeManager.UI/Data/DataAccess/IOrdersDataAccess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGTradeManager.UI.Data.DataAccess
+{
+    public interface IOrdersDataAccess
+    {
+
+    }
+}
